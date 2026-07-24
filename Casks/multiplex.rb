7e6b02e73789a3a1ac8698f240a85b04bd5a39a1
raja <linux1@zoho.com>
2026-07-24 22:32:26 +0530

    multiplex 0.9.0

--- Casks/multiplex.rb
+++ Casks/multiplex.rb
@@ -1,6 +1,6 @@
 cask "multiplex" do
-  version "0.8.0"
-  sha256 "8c3a3c10fc5d4db9197d35db14e32e1488fa50482020cdced8b39dc5ccebf7d9"
+  version "0.9.0"
+  sha256 "3aa6932439b8bc97311b03ec9f1bd1fa3cf51db2f1f6ecbda8d912f3c25ac046"
 
   url "https://github.com/raja-jamwal/homebrew-tap/releases/download/v#{version}/Multiplex-#{version}.dmg"
   name "Multiplex"
