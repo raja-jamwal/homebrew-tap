cf8e28d5878ad9f456f54870808568f634ea48ca
raja <linux1@zoho.com>
2026-08-22 11:57:48 +0530

    Bump multiplex to 0.11.1

--- Casks/multiplex.rb
+++ Casks/multiplex.rb
@@ -1,6 +1,6 @@
 cask "multiplex" do
-  version "0.11.0"
-  sha256 "6a81d2ee6b5381aaeaadf27c2b4569a2afa8984b02fc49ff59635fca581dfaf7"
+  version "0.11.1"
+  sha256 "d043cb508e3be3144adbb868ae0c46893652e39907b0bf82172871d833e0ae64"
 
   url "https://github.com/raja-jamwal/homebrew-tap/releases/download/v#{version}/Multiplex-#{version}.dmg"
   name "Multiplex"
